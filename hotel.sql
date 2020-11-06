a6d162ebc5b503c9c9d0091153e7da4cf67da1a4
Anthony Do <anthony4do@gmail.com>
2020-11-05 23:42:46 -0800

    first commit

---
+++ hotel.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS HOTEL;
+CREATE DATABASE HOTEL;
+USE HOTEL;
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User
+(
+    uID INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    phone VARCHAR(15),
+    age INT,
+    PRIMARY KEY (uid)
+);
+
+DROP TABLE IF EXISTS Room;
+CREATE TABLE Room
+(
+    rID INT,
+    floorNumber INT,
+    reserved BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (roomNumber)
+);
+
+DROP TABLE IF EXISTS Booking;
+CREATE TABLE Booking
+(
+    uID INT,
+    rID INT,
+    startDate DATE,
+    endDate DATE,
+    PRIMARY KEY (uID, rID)
+)
